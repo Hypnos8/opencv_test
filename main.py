dd5420ec328b7553050777a58c06c883d908dc9b
Hypnos8 <justsleeping22@gmail.com>
2022-05-08 13:13:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Note opencv uses BGR not RGB!
+
+import cv2 as cv
+import numpy as np
+
+
+def draw_line_automatically(img):
+    """ Using built-in function"""
+    cv.line(img, (0,0), (500,500), (255,0,0), 1)
+
+
+def draw_line_slowly(img):
+    """Accessing each value using python = operator"""
+    for i in range(0,500):
+        img[i,i] = [255,0,0]
+
+
+def draw_line_itemset(img):
+    """Using itemset"""
+    for i in range(0,500):
+        img.itemset((i,i,0), 255)
+        img.itemset((i,i,1), 0)
+        img.itemset((i,i,2), 0)
+
+def copy_can(img):
+    can = img[0:150, 250:500]
+    img[0:150, 0:250] = can
+
+
+sample_image = cv.imread("sample.jpg")
+sample_image = cv.resize(sample_image, (500,500))
+copy_can(sample_image)
+
+print(sample_image.shape)
+cv.imshow("Sample", sample_image)
+k = cv.waitKey(0)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
